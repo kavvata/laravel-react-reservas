dbc6319124109ee50ea38cf7e234e16425eebe2c
kavvata <gabriel.kawata@gmail.com>
2024-06-13 20:45:07 -0300

    chore: limpando copia e cola

--- resources/js/Pages/Reservaveis/Edit.tsx
+++ resources/js/Pages/Reservaveis/Edit.tsx
@@ -23,10 +23,10 @@ export default function Create({ auth, reservavelJson }: PageProps<{ reservavelJ
     return (
         <AuthenticatedLayout
             user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Novo reservavel</h2>}
+            header={<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Editar Reservavel</h2>}
         >
 
-            <Head title="Novo Reservavel" />
+            <Head title="Editar Reservavel" />
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
