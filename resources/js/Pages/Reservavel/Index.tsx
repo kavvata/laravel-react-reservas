58486213990043fa56c3ee0d3db2ccad82540a24
kavvata <gabriel.kawata@gmail.com>
2024-06-17 17:35:24 -0300

    array.map() just changed my life

--- resources/js/Pages/Reservavel/Index.tsx
+++ resources/js/Pages/Reservavel/Index.tsx
@@ -1,30 +1,29 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head, Link } from '@inertiajs/react';
 import { PageProps } from '@/types';
-import { ReactNode } from 'react';
+
+function CategoriaSection(prop: { categoria: Categoria }) {
+    return (
+        <section>
+            <h2 className="text-lg font-bold mb-2 text-gray-900 dark:text-gray-100">{prop.categoria.nome}</h2>
+            {
+                prop.categoria.reservaveis.map(reservavel => (
+                    <li>
+                        <Link href={route('reservaveis.edit', reservavel.id)}>
+                            {reservavel.nome} - {reservavel.isReservado ? "Reservado" : "Disponivel"}
+                        </Link>
+
+                    </li>
+                )
+                )
+            }
+            <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+        </section>
+    )
+}
 
 export default function Reservaveis({ auth, reservaveisJson }: PageProps<{ reservaveisJson: string }>) {
-    const categorias: any[] = JSON.parse(reservaveisJson);
-
-    const reservaveisElementList: ReactNode[] = [];
-    categorias.forEach((categoria) => {
-
-        // header da categoria
-        reservaveisElementList.push(
-            <h2 className="text-lg font-bold text-gray-900 dark:text-gray-100">{categoria.nome}</h2>
-        )
-
-        // lista de reservaveis da categoria
-        categoria.reservaveis.forEach((reservavel: any) => {
-            reservaveisElementList.push(
-                <li>
-                    <Link href={route('reservaveis.edit', reservavel.id)}>
-                        {reservavel.nome} - {reservavel.isReservado ? "Reservado" : "Disponivel"}
-                    </Link>
-                </li>
-            )
-        });
-    });
+    const categorias: Categoria[] = JSON.parse(reservaveisJson);
 
     return (
         <AuthenticatedLayout
@@ -37,8 +36,10 @@ export default function Reservaveis({ auth, reservaveisJson }: PageProps<{ reser
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                         <ul className="p-6 text-gray-900 dark:text-gray-100">
-                            {reservaveisElementList}
                             <li>
+                                {
+                                    categorias.map(categoria => (<CategoriaSection categoria={categoria} />))
+                                }
                                 <Link className="font-bold" href={route('reservaveis.create')}>
                                     + Novo Reservavel!
                                 </Link>
