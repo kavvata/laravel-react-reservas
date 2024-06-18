44daa7a49b354af27729ad50640a540de5f4526c
kavvata <gabriel.kawata@gmail.com>
2024-06-17 21:14:57 -0300

    type for reserva

---
+++ resources/js/types/models/reserva.ts
@@ -0,0 +1,9 @@
+type Reserva = {
+    id: number,
+    reservavel_id: number,
+    user_id: number,
+    inicio: Date,
+    devolucao_prevista: Date,
+    devolucao: Date | undefined,
+    descricao: string
+}
