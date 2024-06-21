7a137ddd85716bbe285ba00bee46f3849f026f4e
kavvata <gabriel.kawata@gmail.com>
2024-06-20 21:59:14 -0300

    categorias for create page of reservavel

--- resources/js/Pages/Reservavel/Create.tsx
+++ resources/js/Pages/Reservavel/Create.tsx
@@ -5,10 +5,15 @@ import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { PageProps } from "@/types";
 import { Head, useForm } from "@inertiajs/react";
 import { FormEventHandler } from "react";
+import Select from "react-dropdown-select";
 
-export default function Create({ auth }: PageProps) {
+export default function Create({
+    auth,
+    categorias,
+}: PageProps<{ categorias: Categoria[] }>) {
     const { data, setData, post, processing } = useForm({
         nome: "",
+        categoria_id: categorias[0].id,
     });
 
     const submit: FormEventHandler = (e) => {
@@ -29,11 +34,11 @@ export default function Create({ auth }: PageProps) {
             <Head title="Novo Reservavel" />
 
             <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div className="max-w-4xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                         <form onSubmit={submit}>
                             <div className="p-6 space-y-4 text-gray-900 dark:text-gray-100">
-                                <div id="inputs">
+                                <div id="nome-input">
                                     <InputLabel
                                         htmlFor="nome-reservavel"
                                         value="Nome"
@@ -49,6 +54,35 @@ export default function Create({ auth }: PageProps) {
                                         onChange={(e) =>
                                             setData("nome", e.target.value)
                                         }
+                                    />
+                                </div>
+                                <div id="categoria-input">
+                                    <InputLabel
+                                        htmlFor="categoria-select"
+                                        value="Categoria"
+                                    />
+                                    {/* FIXME: this select is bad */}
+                                    <Select
+                                        className="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm "
+                                        multi={false}
+                                        options={categorias}
+                                        values={categorias.filter(
+                                            (c) => c.id == data.categoria_id,
+                                        )}
+                                        onChange={(newValues: Categoria[]) => {
+                                            if (newValues.length == 0) {
+                                                return;
+                                            }
+                                            setData(
+                                                "categoria_id",
+                                                newValues[0].id,
+                                            );
+                                        }}
+                                        labelField="nome"
+                                        valueField="id"
+                                        searchBy="nome"
+                                        placeholder="Sem Categoria"
+                                        color="#111827"
                                     />
                                 </div>
                                 <PrimaryButton disabled={processing}>
